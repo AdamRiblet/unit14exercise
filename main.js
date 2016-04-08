a666174684f6249fd7c7032c05e576774ab227f0
AdamRiblet <adamriblet@yahoo.com>
2016-04-08 00:05:09 -0500

    Exercise14

---
+++ main.js
@@ -0,0 +1,85 @@
+
+
+console.log("Hello, My Name Is Adam Riblet");
+console.log("This is a 'signle quoted statement' and this is a \"double quoted statment!!");
+
+//5a
+console.log(" 2 plus 2 equals: " + (2 + 2));
+
+//5b
+console.log("56 divided by 7 equals: " + (56 / 7));
+//5c
+console.log("42.6 times 18.3 equals: " + (42.6 * 18.3));
+//5d
+console.log("$83.97 miuns $34.86 equals:" + (83.97 - 34.86));
+//5e
+console.log("49 mod 8 equals:" + (49 % 8));
+
+//6a
+console.log("5+4+3/2*6-1 equals:" + (5 + 4 + 3 / 2 * 6 - 2));
+//6b
+console.log("29*75+32/8 equals:" + (29 * 75 + 32 / 8));
+//6c
+console.log("(42 + 89) * 3) / (72-36)equals:" + ((42 + 89) * 3) / (72 - 36));
+//6d
+console.log("4 % 3 equals: " + (4 % 2));
+//6e
+console.log("33.00-15.87 equals: " + (33.00 - 15.87));
+//7 using type of
+//7a
+console.log("The type of '56' is:" + typeof (23));
+//7b
+console.log("The type of '43.211' is: " + typeof (43.211));
+//7c
+console.log('The type of "2" is:' + typeof (2))
+//7d
+console.log('The type of "two" is: ' + typeof (two));
+//7e
+console.log("The type of'newDate() is: " + typeof (newDate));
+//7f
+console.log("The type of'[]' is: " + typeof ([]));
+//7g
+console.log("The type of '{}' is: " + typeof ({}));
+//7h
+console.log("The type of'true' is: " + typeof (true));
+//7i
+console.log("the type of'false' is: " + typeof (false));
+//7j
+console.log("the type of'Null' is: " + typeof (Null));
+//7k
+console.log("The type of 'Undefined' is: " + typeof (Undefined));
+//7l
+console.log("The type of 'NaN' is: " + typeof (Nan));
+//7m
+console.log("The type of '0' is: " + typeof (0));
+//7n
+console.log('the type of"0" is:' + typeof (0));
+//7o
+console.log('the tpye of "" is: ' + typeof (""));
+//7p
+console.log(' the type of""||1 is: ' + typeof ("" || 1));
+//7q
+console.log("the type of '3 > 2' is: " + typeof (3 > 2));
+//7r
+console.log("the type of '2 > 3' is: " + typeof (2 > 3));
+//7s
+console.log(" the type of 'newNumber' (\"some text\") is: " + typeof (new Number("some text")));
+
+//decalre variable
+//8 a
+var foo = 2;
+var bar = 3;
+//8b
+var total = 5;
+//8c
+var answer = total = foo * bar + total;
+console.log(answer);
+//8d
+var answer = total = total / bar;
+console.log(answer);
+//8e
+var answer = total = total ^ 2;
+console.log(answer);
+
+var answer = "The final total is: XX when the starting values are 'foo' YYYY :\ and 'bar': ZZZ";
+console.log(answer);
